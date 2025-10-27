cdd05e862e7d82556c531b9ee87bb05a5b357d38
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-10-26 20:50:41 -0400

    feat(week-1): implement pounds to kilograms weight converter using Node.js
    - Implement command-line argument validation

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,25 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: Jarren Bess
+ * Date: Oct 24, 2025
+ * File Name: weight-converter.js
+ * Description: Node.js script that converts pounds to kilograms
+ */
 
 "use strict";
 
-// TODO: Implement the weight conversion logic here
+function lbsToKgs() {
+  const lbs = process.argv[2];
+  if (process.argv.length != 3) {
+    console.error("Usage: node weight-converter.js <pounds>");
+    process.exit(1);
+  } else if (isNaN(lbs)) {
+    console.error("Input must be a number");
+    process.exit(1);
+  }
+
+  // Convert pounds to kilograms
+  let lbsToKgs = lbs * 0.453592;
+  console.log(`${lbsToKgs.toFixed(2)}`);
+}
+
+lbsToKgs();
